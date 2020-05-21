54d29c0e8512a862e108b4043ad46ebf05232796
unknown <igor.nepovim@zoot.cz>
2020-05-21 15:56:42 +0200

    First commit

---
+++ precache-manifest.e9e01373724fc1681ac98b664145659a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f2a8dd204a989173beb105173a01e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76682a5c14b5b7d762fb",
+    "url": "/static/css/2.5fd2c8ae.chunk.css"
+  },
+  {
+    "revision": "f0e2d4fdda0380fb4139",
+    "url": "/static/css/main.1d68e36d.chunk.css"
+  },
+  {
+    "revision": "76682a5c14b5b7d762fb",
+    "url": "/static/js/2.f460e3b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f460e3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e2d4fdda0380fb4139",
+    "url": "/static/js/main.a6200571.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
